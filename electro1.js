acacc8f2af0c050910cfdfb9f25be35c0dfae623
TheUniqueDeveloper <drag0nislegendary@gmail.com>
2024-05-17 16:47:48 +0545

    day 5

--- electro1.js
+++ electro1.js
@@ -1,3 +1,4 @@
+
 /*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById("nav-menu"),
   navToggle = document.getElementById("nav-toggle"),
@@ -43,32 +44,86 @@ window.addEventListener('scroll', scrollHeader)
 
 /*======================== testimonial swiper =========================*/
 
-let testimonialSwiper = new Swiper(".testimonial-swiper", {
-spaceBetween:30,
-loop:true,
+ let testimonialSwiper = new Swiper(".testimonial-swiper", {
+  spaceBetween:30,
+  loop:true,
 
 
- navigation: {
-   nextEl: ".swiper-button-next",
-   prevEl: ".swiper-button-prev",
- },
-});
+   navigation: {
+     nextEl: ".swiper-button-next",
+     prevEl: ".swiper-button-prev",
+   },
+ });
 
 /*==============================  New swiper==================================== */
 
 let newSwiper = new Swiper(".new-swiper", {
-spaceBetween: 24,
-loop: true,
+  spaceBetween: 24,
+  loop: true,
 
-breakpoints: {
-  576: {
-    slidesPerView: 2,
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
   },
-  768: {
-    slidesPerView: 3,
-  },
-  1024: {
-    slidesPerView: 4,
-  },
-},
 });
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 400 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 400) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+
+
+/*============================SHOW CART===========================*/
+
+const cart = document.getElementById("cart"),
+  cartShop = document.getElementById("cart-shop"),
+  cartClose = document.getElementById("cart-close");
+
+/*===== CART SHOW =====*/
+/* Validate if constant exists */
+if (cartShop) {
+  cartShop.addEventListener("click", () => {
+    cart.classList.add("show-cart");
+  });
+}
+
+/*===== CART HIDDEN =====*/
+/* Validate if constant exists */
+if (cartClose) {
+  cartClose.addEventListener("click", () => {
+    cart.classList.remove("show-cart");
+  });
+}
